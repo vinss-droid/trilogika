0d2412ae942c30b7aa147ca55c1041373a8cdce4
riskhanyaa <riskhanyaa1155@gmail.com>
2023-10-19 14:04:35 +0700

    contact us update

---
+++ resources/views/contact_us.blade.php
@@ -0,0 +1,345 @@
+@extends('layouts.main')
+@section('style')
+    <style>
+        footer {
+            background-image: url("{{ asset('front/assets') }}/images/meetings-bg.jpg");
+            background-position: center center;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        section.heading-page {
+            background-image: url('{{ asset('storage/images/article') }}/{{ $article->image }}');
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            padding-top: 230px;
+            padding-bottom: 110px;
+            text-align: center;
+            filter: grayscale(70%) brightness(50%);
+        }
+
+        .meeting-single-item .down-content p.description {
+            margin-top: 0px;
+            padding-top: 0px;
+            border-top: 1px solid #eee;
+            margin-bottom: 0px;
+            padding-bottom: 0px;
+            border-bottom: 1px solid #eee;
+        }
+
+        .social {
+            top: 5px;
+            position: relative;
+            margin: 0;
+        }
+
+        .list-unstyled {
+            padding-left: 0;
+            list-style: none;
+        }
+
+        /* Google Font CDN Link */
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Poppins", sans-serif;
+        }
+
+        body {
+            min-height: 100vh;
+            width: 100%;
+            background: #ffff;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .container {
+            width: 85%;
+            background: #fff;
+            border-radius: 6px;
+            padding: 20px 60px 30px 40px;
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+        }
+
+        .container .content {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .container .content .left-side {
+            width: 25%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top: 15px;
+            position: relative;
+        }
+
+        .content .left-side::before {
+            content: '';
+            position: absolute;
+            height: 70%;
+            width: 2px;
+            right: -15px;
+            top: 50%;
+            transform: translateY(-50%);
+            background: #1F262C;
+        }
+
+        .content .left-side .details {
+            margin: 14px;
+            text-align: center;
+        }
+
+        .content .left-side .details i {
+            font-size: 30px;
+            color: #1F262C;
+            margin-bottom: 10px;
+        }
+
+        .content .left-side .details .topic {
+            font-size: 18px;
+            font-weight: 500;
+        }
+
+        .content .left-side .details .text-one,
+        .content .left-side .details .text-two {
+            font-size: 14px;
+            color: #1F262C;
+        }
+
+        .container .content .right-side {
+            width: 75%;
+            margin-left: 75px;
+        }
+
+        .content .right-side .topic-text {
+            font-size: 23px;
+            font-weight: 600;
+            color: #1F262C;
+        }
+
+        .right-side .input-box {
+            height: 50px;
+            width: 100%;
+            margin: 12px 0;
+        }
+
+        .right-side .input-box input,
+        .right-side .input-box textarea {
+            height: 100%;
+            width: 100%;
+            border: none;
+            outline: none;
+            font-size: 16px;
+            background: #f1f1f1;
+            border-radius: 6px;
+            padding: 0 15px;
+            resize: none;
+        }
+
+        .right-side .message-box {
+            min-height: 110px;
+        }
+
+        .right-side .input-box textarea {
+            padding-top: 6px;
+        }
+
+        .right-side .button {
+            display: inline-block;
+            margin-top: 12px;
+        }
+
+        .right-side .button input[type="button"] {
+            color: #fff;
+            font-size: 18px;
+            outline: none;
+            border: none;
+            padding: 8px 16px;
+            border-radius: 6px;
+            background: #1F262C;
+            cursor: pointer;
+            transition: all 0.3s ease;
+        }
+
+        .button input[type="button"]:hover {
+            background: #1F262C;
+        }
+
+        @media (max-width: 950px) {
+            .container {
+                width: 90%;
+                padding: 30px 40px 40px 35px;
+            }
+
+            .container .content .right-side {
+                width: 75%;
+                margin-left: 55px;
+            }
+        }
+
+        @media (max-width: 820px) {
+            .container {
+                margin: 40px 0;
+                height: 100%;
+            }
+
+            .container .content {
+                flex-direction: column-reverse;
+            }
+
+            .container .content .left-side {
+                width: 100%;
+                flex-direction: row;
+                margin-top: 40px;
+                justify-content: center;
+                flex-wrap: wrap;
+            }
+
+            .container .content .left-side::before {
+                display: none;
+            }
+
+            .container .content .right-side {
+                width: 100%;
+                margin-left: 0;
+            }
+        }
+    </style>
+@endsection
+@section('content')
+    <section class="heading-page header-text" id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h6>1</h6>
+                    <h2>1</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="contactus-page" id="contactus">
+        <div class="container">
+            <div class="row">
+                <!DOCTYPE html>
+                <html lang="en">
+
+                <head>
+                    <title></title>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+                    <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css"
+                        integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q"
+                        crossorigin="anonymous" />
+
+                    <link href="" rel="stylesheet" />
+
+                    <style>
+
+                    </style>
+                </head>
+
+                <body>
+                    <section class="heading-page header-text" id="top">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <h6>1</h6>
+                                    <h2>1</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section class="ftco-section" id="meetings">
+                        <div class="container align-items-center justify-content-cneter">
+                            <div class="content">
+                                <div class="left-side">
+                                    <div class="address details">
+                                        <i class="fas fa-map-marker-alt"></i>
+                                        <div class="topic">Alamat</div>
+                                        <div class="text-one">Jl. Lingkar Barat Siliwangi, Nogosaren, RT.05/RW.22</div>
+                                        <div class="text-two">Nogotirto, Gamping, Sleman, D.I. Yogyakarta</div>
+                                    </div>
+                                    <div class="phone details">
+                                        <i class="fas fa-phone-alt"></i>
+                                        <div class="topic">Telp.</div>
+                                        <div class="text-one"><a href="tel://+6285701246319">0857-0124-6319</a></div>
+                                    </div>
+                                    <div class="email details">
+                                        <i class="fas fa-envelope"></i>
+                                        <div class="topic">Email</div>
+                                        <div class="text-one"><a
+                                                href="trilogikaedutamayk@gmail.com">trilogikaedutamayk@gmail.com</a></div>
+                                    </div>
+                                </div>
+                                <div class="right-side">
+                                    <div class="topic-text">Send us a message</div>
+                                    <p>If you have any work from me or any types of quries related to my tutorial, you can
+                                        send me message from here. It's my pleasure to help you.</p>
+                                    <form action="#">
+                                        <div class="input-box">
+                                            <input type="text" placeholder="Enter your name" />
+                                        </div>
+                                        <div class="input-box">
+                                            <input type="text" placeholder="Enter your email" />
+                                        </div>
+                                        <div class="input-box message-box">
+                                            <textarea placeholder="Enter your message"></textarea>
+                                        </div>
+                                        <div class="button">
+                                            <input type="button" value="Send Now" />
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <!-- maps -->
+
+                            <div class="content" style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+                                <div class="container" style="text-align: center;">
+                                    <div id="map" style="width: 100%; height: 500px;"></div>
+                                    <script>
+                                        // Panggil fungsi initMap saat halaman dimuat
+                                        google.maps.event.addDomListener(window, 'load', initMap);
+                                    </script>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+                    </script>
+                    <script>
+                        function initMap() {
+                            var map = new google.maps.Map(document.getElementById('map'), {
+                                center: {
+                                    lat: -7.774349,
+                                    lng: 110.331117
+                                }, // Koordinat tengah peta
+                                zoom: 14 // Perbesar level peta (semakin tinggi, semakin dekat)
+                            });
+                        }
+                    </script>
+
+                    <script>
+                        // Panggil fungsi initMap saat halaman dimuat
+                        google.maps.event.addDomListener(window, 'load', initMap);
+                    </script>
+                </body>
+
+                </html>
+            @endsection
